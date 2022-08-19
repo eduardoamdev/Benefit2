dbc15013315259dbc69fe084212a89f5bdcf18c9
eduardoamdev <eduardoamdev@gmail.com>
2022-08-19 14:22:58 +0200

    Ownable and first modifiers added

--- contracts_enviroment/contracts/Benefit.sol
+++ contracts_enviroment/contracts/Benefit.sol
@@ -2,19 +2,34 @@
 pragma solidity ^0.8.0;
 
 import "./ERC20.sol";
+import "./Ownable.sol";
 
-contract Benefit is ERC20 {
-    address public owner;
+contract Benefit is ERC20, Ownable {
     address public contractAddress;
     uint256 public initialPrice;
     bool public initialPriceStablished;
     bool public initialMintAvailable;
 
     constructor() ERC20("Benefit", "BNF") {
-        owner = msg.sender;
         contractAddress = address(this);
         initialPriceStablished = false;
         initialMintAvailable = true;
+    }
+
+    modifier checkInitialPriceStablished() {
+        require(
+            initialPriceStablished == true,
+            "Initial price must be established"
+        );
+        _;
+    }
+
+    modifier checkInitialMintAvailable() {
+        require(
+            initialMintAvailable == true,
+            "Initial mint has already been executed"
+        );
+        _;
     }
 
     function setInitialPrice(uint256 _initialPrice) public {
@@ -22,29 +37,18 @@ contract Benefit is ERC20 {
         initialPriceStablished = true;
     }
 
-    function initialMint(uint256 _amount) public payable {
-        require(
-            initialPriceStablished == true,
-            "Initial price must be established"
-        );
-        require(initialMintAvailable == true, "Initial mint must be available");
-        require(
-            msg.sender == owner,
-            "This addres is not allowed to mint BNF tokens"
-        );
-        require(
-            msg.sender.balance >= msg.value,
-            "Yo have not enought funds to mint that Tokens"
-        );
+    function initialMint(uint256 _amount)
+        public
+        payable
+        onlyOwner
+        checkInitialMintAvailable
+        checkInitialPriceStablished
+    {
         require(
             msg.value == _amount * initialPrice,
             "Value does not match with tokens amount cost"
         );
         _mint(contractAddress, _amount);
         initialMintAvailable = false;
-    }
-
-    function getContractBalance() public view returns (uint256) {
-        return balanceOf(contractAddress);
     }
 }
